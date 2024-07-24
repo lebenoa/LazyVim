5635e210a6de8e17ff43a614a007ddaa0158fc57
Meonako <dubi.db28@gmail.com>
2024-07-24 11:53:45 +0700

    Feat: new keybinds for neovide

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 require("config.lazy")
 
 if vim.g.neovide then
-  vim.g.neovide_transparency = 0.9
-  vim.o.guifont = "IosevkaTerm Nerd Font"
+    vim.g.neovide_transparency = 0.8
+    vim.neovide_font_size = 15
+    vim.o.guifont = "IosevkaTerm Nerd Font:h" .. vim.neovide_font_size
 end
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,50 @@
 
 vim.keymap.set({ "n", "v" }, "<C-c>", "<cmd>normal gcc<cr>", { desc = "Toggle comment current line" })
 vim.keymap.set({ "n", "v" }, "U", "<CS-r>", { desc = "Redo" })
+
+if vim.g.neovide then
+    local transparency_notification
+    local font_notification
+
+    vim.keymap.set({ "n", "v" }, "<C-kLeft>", function()
+        vim.g.neovide_transparency = vim.g.neovide_transparency - 0.05
+        transparency_notification = vim.notify("Transparency: " .. vim.g.neovide_transparency, "info", {
+            replace = transparency_notification,
+        })
+    end, { desc = "Increase transparency" })
+    vim.keymap.set({ "n", "v" }, "<C-kRight>", function()
+        vim.g.neovide_transparency = vim.g.neovide_transparency + 0.05
+        transparency_notification = vim.notify("Transparency: " .. vim.g.neovide_transparency, "info", {
+            replace = transparency_notification,
+        })
+    end, { desc = "Reduce transparency" })
+
+    vim.keymap.set({ "n", "v" }, "<C-kPlus>", function()
+        local prev_font_size = vim.neovide_font_size
+        vim.neovide_font_size = prev_font_size + 1
+
+        if string.find(vim.o.guifont, ":h", 1, true) then
+            vim.o.guifont = string.gsub(vim.o.guifont, ":h" .. prev_font_size, ":h" .. vim.neovide_font_size)
+        else
+            vim.o.guifont = vim.o.guifont .. ":h" .. vim.neovide_font_size
+        end
+
+        font_notification = vim.notify("Font Size: " .. vim.neovide_font_size, "info", {
+            replace = font_notification,
+        })
+    end, { desc = "Increase font size" })
+    vim.keymap.set({ "n", "v" }, "<C-kMinus>", function()
+        local prev_font_size = vim.neovide_font_size
+        vim.neovide_font_size = prev_font_size - 1
+
+        if string.find(vim.o.guifont, ":h", 1, true) then
+            vim.o.guifont = string.gsub(vim.o.guifont, ":h" .. prev_font_size, ":h" .. vim.neovide_font_size)
+        else
+            vim.o.guifont = vim.o.guifont .. ":h" .. vim.neovide_font_size
+        end
+
+        font_notification = vim.notify("Font Size: " .. vim.neovide_font_size, "info", {
+            replace = font_notification,
+        })
+    end, { desc = "Reduce font size" })
+end
